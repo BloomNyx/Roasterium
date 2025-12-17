fb4d6f22e50db09d73e76ff09ab203a350a25b56
BloomNyx <bagsuhyeon507@gmail.com>
2025-12-17 17:37:41 +0900

    cozy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-export default {
-  root: "src",
+// vite.config.js
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  root: "src",          // 소스 루트
+  base: "/Roasterium/", // GitHub Pages 배포 경로
   build: {
-    outDur:"../public",
+    outDir: "../dist",  // 빌드 결과물이 루트 기준 dist 폴더에 생성
+    emptyOutDir: true,  // 기존 dist 폴더 내용 제거
   },
-};
+})
